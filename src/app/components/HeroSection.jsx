78adcfed1bdae9eef0fca405563ec041e8622167
GhugareSanika <144719999+GhugareSanika@users.noreply.github.com>
2024-07-06 22:54:32 +0530

    Added Homepage

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -7,10 +7,11 @@ const HeroSection = () => {
     <section>
         <div className="grid grid-cols-1 sm:grid-cols-12">
             <div className="col-span-7 place-self-center text-center sm:text-left">
-                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold mt-8">
                     <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-700">
                         Hello,I'm {" "}
                     </span>
+                    <br/>
                     <TypeAnimation
                         sequence={[
                             "Sanika",
