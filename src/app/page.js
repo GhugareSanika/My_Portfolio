3f5aac1e2b3ec3f04ea84357ab608184fb0de343
GhugareSanika <144719999+GhugareSanika@users.noreply.github.com>
2024-07-25 00:29:59 +0530

    Commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,8 @@ import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import ProjectSection from "./components/ProjectSection";
-import ProjectCard from "./components/ProjectCard";
+import EmailSection from "./components/EmailSection";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -12,7 +13,9 @@ export default function Home() {
         <HeroSection/>
         <AboutSection/>
         <ProjectSection/>
+        <EmailSection/>
       </div>
+      <Footer/>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +11,26 @@ module.exports = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        ...colors,
+        primary: colors.pink,
+        secondary: colors.blue,
+      },
+      boxShadow: {
+        'custom-shadow': '4px 4px 6px rgba(255, 0, 0, 0.5)',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function({ addUtilities }) {
+      const newUtilities = {
+        '.scroll-smooth': {
+          scrollBehavior: 'smooth',
+        },
+      };
+      addUtilities(newUtilities, ['responsive']);
+    },
+  ],
 };
